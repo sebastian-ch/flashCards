f57a0f6b11d254eb53f29a13dd80471aef45abe0
Sebastian Hancock <sebastian.hancock@tum.de>
2020-02-02 16:39:54 +0100

    more verbs

---
+++ verbs.js
@@ -0,0 +1,57 @@
+var vocab = {
+    0: {
+        verb: "müssen",
+        ich: "muss",
+        du: "musst",
+        er_sie_es: "muss",
+        wir: "müssen",
+        ihr: "müsst",
+        Sie: "müssen"
+    },
+    1: {
+        verb: "haben",
+        ich: "habe",
+        du: "hast",
+        er_sie_es: "hat",
+        wir: "haben",
+        ihr: "habt",
+        Sie: "haben"
+
+    },
+    2: {
+        verb: "können",
+        ich: "kann",
+        du: "kannst",
+        er_sie_es: "kann",
+        wir: "können",
+        ihr: "könnt",
+        Sie: "können"
+    },
+    3: {
+        verb: "sollen",
+        ich: "soll",
+        du: "sollst",
+        er_sie_es: "soll",
+        wir: "sollen",
+        ihr: "sollt",
+        Sie: "sollen"
+    },
+    4: {
+        verb: "wollen",
+        ich: "will",
+        du: "willst",
+        er_sie_es: "will",
+        wir: "wollen",
+        ihr: "wollt",
+        Sie: "wollen"
+    },
+    5: {
+        verb: "dürfen",
+        ich: "darf",
+        du: "darfst",
+        er_sie_es: "darf",
+        wir: "dürfen",
+        ihr: "dürft",
+        Sie: "dürfen"
+    }
+}
